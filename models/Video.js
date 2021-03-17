be6b5492c70610f8326d4674796e27aea0522ad4
Tae.Garam <drasla@naver.com>
2021-03-17 17:02:25 +0900

    #3.4 Video Model

--- models/Video.js
+++ models/Video.js
@@ -20,6 +20,10 @@ const VideoSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
+    comments: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }
 });
 
 const model = mongoose.model("Video", VideoSchema);
