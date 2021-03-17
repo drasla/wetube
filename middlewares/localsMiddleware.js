d9783d9d7172b4a86153705f543afa675d4a9014
Tae.Garam <drasla@naver.com>
2021-03-17 18:01:01 +0900

    #3.6 Uploading and Creating a Video

--- middlewares/localsMiddleware.js
+++ middlewares/localsMiddleware.js
@@ -1,4 +1,11 @@
+import multer from "multer";
 import routes from "../routes";
+
+const multerVideo = multer({
+    dest: "videos/"
+})
+
+export const uploadVideoMiddleware = multerVideo.single("videoFile");
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import routes from "../routes";
 import {deleteVideo, editVideo, getUpload, postUpload, videoDetail} from "../controllers/videoController";
+import {uploadVideoMiddleware} from "../middlewares/localsMiddleware";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideoMiddleware, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
