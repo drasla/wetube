9d0dceea75babf7a4e0c18812febba897d096500
Tae.Garam <drasla@naver.com>
2021-03-17 15:19:33 +0900

    #2.25 More Controllers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import {videos} from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => res.render("Home", { pageTitle: "Home", videos });
 
@@ -8,10 +9,17 @@ export const search = (req, res) => {
     res.render("Search", { pageTitle: "Search", searchingBy, videos });
 };
 
-export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 
-export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
+export const postUpload = (req, res) => {
+    const { body: { file, title, description }} = req;
 
-export const editVideo = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video" });
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(324393));
+}
 
-export const deleteVideo = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
