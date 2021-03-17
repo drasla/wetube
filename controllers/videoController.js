a7586ad790f80acc28a7d6ec72f4468dda6f461a
Tae.Garam <drasla@naver.com>
2021-03-17 12:17:43 +0900

    #수정

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,11 @@
 export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("Search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    const { query: { term: searchingBy }} = req;
+
+    res.render("Search", { pageTitle: "Search", searchingBy });
+};
+
 export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
 
 export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
