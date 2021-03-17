a2ce314d331dfa3333bda590f5767050ac11c158
Tae.Garam <drasla@naver.com>
2021-03-17 19:10:50 +0900

    #3.8 Getting Video by ID + #3.9 Editing a Video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,24 @@
 import express from "express";
 import routes from "../routes";
-import {deleteVideo, editVideo, getUpload, postUpload, videoDetail} from "../controllers/videoController";
+import {
+    deleteVideo,
+    getEditVideo,
+    getUpload,
+    postEditVideo,
+    postUpload,
+    videoDetail
+} from "../controllers/videoController";
 import {uploadVideoMiddleware} from "../middlewares/localsMiddleware";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideoMiddleware, postUpload);
+
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
