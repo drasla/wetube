098a9b9fc1bcbe34cc5980a466715f02fe6bda0f
Tae.Garam <drasla@naver.com>
2021-06-02 23:39:04 +0900

    #7.5 Login Part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,20 +24,35 @@ export const postJoin = async (req, res) => {
     if(exists) {
         return res.status(400).render("join", { pageTitle, errorMessage: "This username/email is already taken." });
     }
-
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location
-    });
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location
+        });
+    } catch (e) {
+        return res.status(400).render("join", { pageTitle, errorMessage: e})
+    }
 
     res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle: "Log In" });
+}
+export const postLogin = async (req, res) => {
+    const { username, password } = req.body;
+
+    const exists = await User.exists({
+        username
+    });
+    if(!exists) {
+        return res.status(400).render("login", { pageTitle: "Log In", errorMessage: "An account with this username does not exists."});
+    }
+}
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
