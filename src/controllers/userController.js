91e4531a7c17dc631059d16803fca48e09e92999
Tae.Garam <drasla@naver.com>
2021-06-02 21:56:17 +0900

    #7.1 Create Account part Two

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,21 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join"});
 }
 
-export const postJoin = (req, res) => {
-    res.render("join", { pageTitle: "Join"});
-}
+export const postJoin = async (req, res) => {
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location
+    });
+
+    res.redirect("/");
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
